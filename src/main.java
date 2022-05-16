0b6fd0dde980353dcfc1836f4ad67a4577575f93
Jacob Bochmann <72769017+wascup@users.noreply.github.com>
2022-05-16 12:40:04 -0500

    App launch and more

--- src/main.java
+++ src/main.java
@@ -1,18 +1,30 @@
+import java.io.File;
 import java.util.Scanner;
 
 public class main {
     public static void main(String[] args) {
-
+        new main();
+    }
+    private String input;
+    public main() {
         try {
+            //ask for user input
             Scanner scan = new Scanner(System.in);
-            busb usb = new busb();
-
-            String input;
 
             System.out.print("\nLocation for the task script: ");
+            //set user input file to input variable
             input = scan.nextLine();
 
-            usb.executeTaskScript(input);
+
+            //check if the file actually exists, if not then restart the program and ask again
+            File fileInput = new File(input);
+            if (!fileInput.exists()) {
+                new main();
+                return;
+            }
+
+            //run the task using the input file
+            new busb().executeTaskScript(input);
         }
         catch(Exception e) {
             System.out.println(e);
